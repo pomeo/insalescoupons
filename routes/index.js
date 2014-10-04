d251cf51484535dfb29da2f97f80964a2a3973a3
Sergey Ovechkin <me@sovechkin.com>
2014-10-04 12:50:39 +0400

    Подключение mongoose

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express     = require('express'),
     router      = express.Router(),
+    mongoose    = require('mongoose'),
+    Schema      = mongoose.Schema,
     winston     = require('winston'),
     Logstash    = require('winston-logstash').Logstash,
     logger      = new (winston.Logger)({
