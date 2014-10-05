2744c89bedf8563207e876d60df1726a137585ae
Sergey Ovechkin <me@sovechkin.com>
2014-10-05 11:55:10 +0400

    время жизни сессии 1 год

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
-app.use(session({ store: new RedisStore({host:'redis.salesapps.ru', port:6379, pass:''}), secret: process.env.SECRET }))
+app.use(session({ store: new RedisStore({host:'redis.salesapps.ru', port:6379, pass:''}), secret: process.env.SECRET, cookie: { maxAge: 31536000000 } }))
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bugsnag.errorHandler);
 
