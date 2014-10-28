270fa812b924767f2772ab608d493d45580f1224
Sergey Ovechkin <me@sovechkin.com>
2014-10-28 11:26:20 +0300

    валидация и отправка формы создания купонов

--- src/js/ui.js
+++ src/js/ui.js
@@ -1,5 +1,54 @@
 $(document).ready(function() {
   sample();
+  $.validator.addMethod('loginRegex', function(value, element) {
+    return this.optional(element) || /^[a-zA-Zа-яА-Я0-9\-]+$/i.test(value);
+  }, 'Только цифры, буквы и дефис');
+  $('#coupons-generate').validate({
+    errorClass: 'uk-form-danger',
+    validClass: 'uk-form-success',
+    highlight: function(element, errorClass, validClass) {
+      $(element).addClass(errorClass).removeClass(validClass);
+    },
+    unhighlight: function(element, errorClass, validClass) {
+      $(element).removeClass(errorClass).addClass(validClass);
+    },
+    rules: {
+      'c-num': {
+        required: true,
+        range: [1, 10000]
+      },
+      'c-part': {
+        required: true,
+        range: [1, 5]
+      },
+      'c-partlen': {
+        required: true,
+        range: [4, 10]
+      },
+      discount: {
+        required: true
+      },
+      until: {
+        required: true,
+        dateNL: true
+      },
+      group: {
+        loginRegex: true
+      }
+    },
+    submitHandler: function(form) {
+      $(form).ajaxSubmit({
+        success: function (response) {
+          if (response == 'success') {
+            $.UIkit.notify("<i class='uk-icon-check'></i> Отправлено в очередь на выполнение.<br />Состояние можно посмотреть на странице <a href='/zadaniya'>&laquo;Задания&raquo;</a>.", {status:'success'})
+          } else {
+            $.UIkit.notify("ошибка", {status:'danger'});
+          }
+        }
+      });
+      return false;
+    }
+  })
   $("input[name='c-part'], input[name='c-partlen']").change(sample);
   function sample() {
     var n = $("input[name='c-num']").val();
