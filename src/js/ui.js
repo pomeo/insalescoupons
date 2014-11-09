cc5319fa4d5518e8aff61fc61b3b7d2b9a24ef35
Sergey Ovechkin <me@sovechkin.com>
2014-11-09 15:20:28 +0300

    тестовая генерация slickgrid

--- src/js/ui.js
+++ src/js/ui.js
@@ -69,4 +69,31 @@ $(document).ready(function() {
       $("#b-discount").prepend("<i class='uk-icon-rub'></i>");
     }
   });
+  var grid;
+  var columns = [
+    {id: "title", name: "Title", field: "title"},
+    {id: "duration", name: "Duration", field: "duration"},
+    {id: "%", name: "% Complete", field: "percentComplete"},
+    {id: "start", name: "Start", field: "start"},
+    {id: "finish", name: "Finish", field: "finish"},
+    {id: "effort-driven", name: "Effort Driven", field: "effortDriven"}
+  ];
+  var options = {
+    enableCellNavigation: true,
+    enableColumnReorder: false
+  };
+  $(function () {
+    var data = [];
+    for (var i = 0; i < 500; i++) {
+      data[i] = {
+        title: "Task " + i,
+        duration: "5 days",
+        percentComplete: Math.round(Math.random() * 100),
+        start: "01/01/2009",
+        finish: "01/05/2009",
+        effortDriven: (i % 5 == 0)
+      };
+    }
+    grid = new Slick.Grid("#coupons", data, columns, options);
+  })
 });
