571b0152305c7865f489f83b3787ac5a42010ae2
Sergey Ovechkin <me@pomeo.me>
2015-01-28 00:26:43 +0300

    FIX не срабатывало

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,15 +10,17 @@ offroad_modules   "defaults", "supervisord"
 set :repository,  "git@github.com:pomeo/insalescoupons.git"
 set :supervisord_start_group, "app"
 set :supervisord_stop_group, "app"
+set :shared_children, shared_children + %w{files}
 #========================
 #ROLES
 #========================
 role :app,        "ubuntu@#{application}"
 
 namespace :deploy do
-  task :symlink, :roles => :app do
-    run "ln -nfs #{shared_path}/files #{release_path}/files"
+  desc "Symlink shared configs and folders on each release."
+  task :symlink_shared do
+    run "ln -s #{shared_path}/files #{release_path}/files"
   end
 end
 
-after "deploy:create_symlink", "deploy:npm_install", "deploy:symlink", "deploy:restart"
+after "deploy:create_symlink", "deploy:npm_install", "deploy:symlink_shared", "deploy:restart"
