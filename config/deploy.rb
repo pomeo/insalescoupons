64206c73905835c7fba4d6dd1dc1303b49173f7d
Sergey Ovechkin <me@pomeo.me>
2015-11-26 14:46:31 +0300

    rewrite capistrano config for new server

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,14 +8,14 @@ set :application, "coupons.salesapps.ru"
 require           "capistrano-offroad"
 offroad_modules   "defaults", "supervisord"
 set :repository,  "git@github.com:pomeo/insalescoupons.git"
-set :deploy_to,   "/home/ubuntu/projects/coupons"
-set :supervisord_start_group, "coupons"
-set :supervisord_stop_group, "coupons"
+set :supervisord_start_group, "app"
+set :supervisord_stop_group, "app"
 set :shared_children, shared_children + %w{files}
 #========================
 #ROLES
 #========================
-role :app,        "ubuntu@#{application}"
+set  :gateway,    "#{application}" # main server
+role :app,        "10.3.20.1"      # container
 
 namespace :deploy do
   desc "Symlink shared configs and folders on each release."
@@ -24,11 +24,8 @@ namespace :deploy do
   end
 end
 
-namespace :deploy do
-  desc "Change node.js port"
-  task :chg_port do
-    run "sed -i 's/3000/3200/g' #{current_path}/app.js"
-  end
-end
-
-after "deploy:create_symlink", "deploy:npm_install", "deploy:symlink_shared", "deploy:chg_port", "deploy:restart"
+after "deploy:create_symlink",
+      "deploy:npm_install",
+      "deploy:symlink_shared",
+      "deploy:cleanup",
+      "deploy:restart"
