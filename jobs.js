fe136b21d09b27197f4a4e7a6d6e1d50e3dfaa80
Sergey Ovechkin <me@pomeo.me>
2015-11-26 16:06:50 +0300

    remove manual garage collector

--- jobs.js
+++ jobs.js
@@ -1642,9 +1642,3 @@ function log(logMsg, logType) {
     logger.info(logMsg);
   }
 };
-
-// server with small memory, need manual release
-setInterval(function () {
-  global.gc();
-  log((process.memoryUsage().rss / 1024 / 1024).toFixed(2) + 'Mb');
-}, 10000);
