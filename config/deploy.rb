e6a85ef61e4fd81bc50fb3fd66ff96a48a73e4a1
Sergey Ovechkin <me@pomeo.me>
2015-11-27 08:56:17 +0300

    change create symlink to files

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,22 +10,14 @@ offroad_modules   "defaults", "supervisord"
 set :repository,  "git@github.com:pomeo/insalescoupons.git"
 set :supervisord_start_group, "app"
 set :supervisord_stop_group, "app"
-set :shared_children, shared_children + %w{files}
+set :shared_children, shared_children + %w{public/files}
 #========================
 #ROLES
 #========================
 set  :gateway,    "#{application}" # main server
 role :app,        "10.3.20.1"      # container
 
-namespace :deploy do
-  desc "Symlink shared configs and folders on each release."
-  task :symlink_shared do
-    run "ln -s #{shared_path}/files #{release_path}/files"
-  end
-end
-
 after "deploy:create_symlink",
       "deploy:npm_install",
-      "deploy:symlink_shared",
       "deploy:cleanup",
       "deploy:restart"
