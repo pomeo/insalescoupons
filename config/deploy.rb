bacff04f0dc7ab6651b1098756fb970faa104374
Sergey Ovechkin <me@pomeo.me>
2016-01-31 21:23:06 +0300

    add shared folder to capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,14 +10,21 @@ offroad_modules   "defaults", "supervisord"
 set :repository,  "git@github.com:pomeo/insalescoupons.git"
 set :supervisord_start_group, "app"
 set :supervisord_stop_group, "app"
-set :shared_children, shared_children + %w{public/files}
 #========================
 #ROLES
 #========================
 set  :gateway,    "#{application}" # main server
 role :app,        "10.3.20.1"      # container
 
+namespace :deploy do
+  desc "Symlink shared configs and folders on each release."
+  task :symlink_shared do
+    run "ln -s #{shared_path}/files #{release_path}/public/files"
+  end
+end
+
 after "deploy:create_symlink",
       "deploy:npm_install",
       "deploy:cleanup",
+      "deploy:symlink_shared",
       "deploy:restart"
